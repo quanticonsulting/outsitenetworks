54cce8d528f28ea66f697223c03fa0717fe8e903
Timothy Dunbar <timothy.c.dunbar@protonmail.com>
2018-04-23 18:57:47 -0400

    0003 complete

--- 2018-04_JQL/tickets/0003/ONI-DEV-TOTAL_ACCOUNTS.js
+++ 2018-04_JQL/tickets/0003/ONI-DEV-TOTAL_ACCOUNTS.js
@@ -1,42 +1,5 @@
-var today = new Date();
-
-function formatDate(date) {
-    
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-
-    return [year, month, day].join('-');
-}
-
-params = {
-  from_date: "2018-01-01",
-    to_date: formatDate(today),
-  event: "Transaction Item"
-}
-
 function main() {
-    return join(
-	Events({
-	    from_date: params.from_date,
-	    to_date: params.to_date,
-            event_selectors: [{
-                event: params.event
-	    }]
-	}),
-        People(), {
-            type: "inner",
-            selectors: [{
-                selector: '(user[".Profile type"] == "Person")'
-            }]
-        })
-        .filter(function(tuple) {
-            return typeof tuple.user.properties["Consumer token: APID"] == "undefined" || typeof tuple.user.properties["Consumer token: APID"] != "undefined";
-	})
+    return People()
 	.groupByUser([function(u) { return u.event.properties["Consumer ID"]}], mixpanel.reducer.count())
 	.groupBy([mixpanel.slice("key", 1)],mixpanel.reducer.sum('value'))
 	.groupBy(["key.1"],mixpanel.reducer.sum('value'))
--- 2018-04_JQL/tickets/0003/ONI-DEV-TOTAL_REGISTERED_ACCOUNTS.js
+++ 2018-04_JQL/tickets/0003/ONI-DEV-TOTAL_REGISTERED_ACCOUNTS.js
@@ -1,43 +1,9 @@
-var today = new Date();
-
-function formatDate(date) {
-    
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-
-    return [year, month, day].join('-');
-}
-
-params = {
-  from_date: "2018-01-01",
-    to_date: formatDate(today),
-  event: "Transaction Item"
-}
-
 function main() {
-    return join(
-	Events({
-	    from_date: params.from_date,
-	    to_date: params.to_date,
-            event_selectors: [{
-                event: params.event
-	    }]
-	}),
-        People(), {
-            type: "inner",
-            selectors: [{
-                selector: '(user[".Profile type"] == "Person")'
-            }]
-        })
+    return People()
         .filter(function(tuple) {
-            return typeof tuple.user.properties["Consumer token: APID"] != "undefined";
+            return typeof tuple.properties["Consumer token: APID"] != "undefined";
 	})
-	.groupByUser([function(u) { return u.event.properties["Consumer ID"]}], mixpanel.reducer.count())
+	.groupByUser([function(u) { return u.properties["Consumer ID"]}], mixpanel.reducer.count())
 	.groupBy([mixpanel.slice("key", 1)],mixpanel.reducer.sum('value'))
 	.groupBy(["key.1"],mixpanel.reducer.sum('value'))
 	.map(function(keysvalues) {
